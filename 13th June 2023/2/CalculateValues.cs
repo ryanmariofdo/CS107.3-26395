d6df927b7550591e3e0ee8254cf77074d635a30b
Ryan Mario <132426286+ryanmariofdo@users.noreply.github.com>
2023-06-13 11:51:43 +0530

    Add files via upload

---
+++ 13th June 2023/2/CalculateValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class CalculateValues
+    {
+        public double sum(double no1, double no2)
+        {
+            return no1 + no2;
+        }
+
+        public double sub(double no1, double no2)
+        {
+            return no1 - no2;
+        }
+
+        public double multi(double no1, double no2)
+        {
+            return no1 * no2;
+        }
+
+        public double divi(double no1, double no2)
+        {
+            return no1 / no2;
+        }
+    }
+}
