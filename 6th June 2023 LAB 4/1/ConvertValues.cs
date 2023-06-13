14c9b3676210a661c32aeaa4319758662c47870d
Ryan Mario <132426286+ryanmariofdo@users.noreply.github.com>
2023-06-13 11:17:19 +0530

    Add files via upload

---
+++ 6th June 2023 LAB 4/1/ConvertValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class ConvertValues
+{
+    public double KilometerToMeter(double kilometers)
+    {
+        double meters = kilometers * 1000;
+        return meters;
+    }
+}
---
+++ 6th June 2023 LAB 4/1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a kilometer value: ");
+        double kilometers = double.Parse(Console.ReadLine());
+
+        ConvertValues obj = new ConvertValues();
+        double meters = obj.KilometerToMeter(kilometers);
+
+        Console.Write(meters.ToString());
+        Console.ReadKey();
+    }
+}
