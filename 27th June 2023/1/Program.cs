e97a22830ce738060a0b6027b3c9d7b2f9be83ee
Ryan Mario <132426286+ryanmariofdo@users.noreply.github.com>
2023-06-27 11:15:00 +0530

    Add files via upload

---
+++ 27th June 2023/1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            int sum = 0;
+
+
+            for (int i = 0; i<num.Length; i++)
+            {
+                Console.Write("Enter value: ");
+                num[i] = int.Parse(Console.ReadLine ());
+            }
+
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                sum = sum + num[i];
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 27th June 2023/2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 122, 45, 65, 56, 49, 326, 15, 45, 945, 153 };
+
+            int smallest = num[0];
+            int biggest = num[0];
+
+            for(int i = 0; i<num.Length; i++)
+            {
+                if (smallest > num[i])
+                {
+                    smallest = num[i];
+                }
+
+                if (biggest < num[i])
+                    biggest = num[i];
+            }
+
+            Console.WriteLine("Smallest: " + smallest);
+            Console.WriteLine("Biggest: " + biggest);
+            Console.ReadKey();
+        }
+    }
+}
