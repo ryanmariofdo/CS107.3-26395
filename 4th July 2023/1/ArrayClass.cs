fd99d1b45f3de2ecedb3b3fa8d2b91759984aaf7
Ryan Mario <132426286+ryanmariofdo@users.noreply.github.com>
2023-07-04 10:55:23 +0530

    Add files via upload

---
+++ 4th July 2023/1/ArrayClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public int[] arrayMethod(int size)
+    {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.Length; i +=2)
+        {
+            Console.WriteLine("Enter a value: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            array[i] = value;
+            array[i + 1] = 0;
+        }
+        return array;
+    }
+}
---
+++ 4th July 2023/1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size = int.Parse(Console.ReadLine());
+
+            ArrayClass obj = new ArrayClass();
+            int[] array = obj.arrayMethod(size);
+
+            Console.WriteLine("Enter values for the array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[1] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
